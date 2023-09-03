3e29fa93ad4e3e42b3dc387caa7f37e941f31866
Raffay Sajjad <raffaysajjad.apple@gmail.com>
2023-09-03 23:48:46 +0500

    Added an option to send feedback

--- src/actions/emailSupport.tsx
+++ src/actions/emailSupport.tsx
@@ -4,8 +4,22 @@ import { BACKEND_URL } from "@/constants/config";
 export async function sendSupportEmail(body: any): Promise<boolean> {
   const API = `${BACKEND_URL}/reelife/support`;
 
-  console.log("API", API);
-  console.log("body", body);
+  return axios
+    .post(API, body)
+    .then((response) => {
+      return response.data;
+    })
+    .then((responseJson) => {
+      return Promise.resolve(responseJson);
+    })
+    .catch((error) => {
+      const result = error.result;
+      return Promise.reject(result);
+    });
+}
+
+export async function sendFeedbackEmail(body: any): Promise<boolean> {
+  const API = `${BACKEND_URL}/reelife/feedback`;
 
   return axios
     .post(API, body)
